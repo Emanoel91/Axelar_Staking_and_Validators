e92b35e7d4e0ea45f16edadb1e58c2d7493497d3
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-08-24 02:56:14 +0330

    Update 2_🥩Staking_Stats.py

--- pages/2_🥩Staking_Stats.py
+++ pages/2_🥩Staking_Stats.py
@@ -18,7 +18,6 @@ st.set_page_config(
 st.title("🥩Staking Stats")
 
 # --- attention ---------------------------------------------------------------------------------------------------------
-st.info("📊Charts initially display data for a default time range. Select a custom range to view results for your desired period.")
 st.info("⏳On-chain data retrieval may take a few moments. Please wait while the results load.")
 
 # --- Sidebar Footer Slightly Left-Aligned ---
